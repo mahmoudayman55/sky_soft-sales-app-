e3bc7e085fb2bbc049c8b065dfb5c3a0f029a2c8
Mahmoud ayman <76877697+mahmoudayman55@users.noreply.github.com>
2022-03-11 22:46:27 +0200

    Update dio_helper.dart

--- skysoft/lib/core/services/dio_helper.dart
+++ skysoft/lib/core/services/dio_helper.dart
@@ -3,6 +3,6 @@ import 'package:dio/dio.dart';
 Dio dioHelper=Dio();
 
 
-String url='http://164.68.105.110';
-String getData='/get_my_data';
-String apiPass='1';
+String url='http://***.**.***.***';
+String getDataRoute='/******';
+String apiPass='****';
